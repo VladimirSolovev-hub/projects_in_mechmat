1f1d9d01a6321ba75a996ac8c9137a457bf8cdd0
Владимир Соловьев <solvov2006@gmail.com>
2024-09-19 13:41:10 +0500

    initial coomit

---
+++ build/CMakeFiles/nagel.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/vladimirsolovev/Documents/prog/homework/2sem/R2Graph.cpp" "CMakeFiles/nagel.dir/R2Graph.cpp.o" "gcc" "CMakeFiles/nagel.dir/R2Graph.cpp.o.d"
+  "/Users/vladimirsolovev/Documents/prog/homework/2sem/nagelpoint.cpp" "CMakeFiles/nagel.dir/nagelpoint.cpp.o" "gcc" "CMakeFiles/nagel.dir/nagelpoint.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
